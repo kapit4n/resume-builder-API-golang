324229d17408d198b98e78230a8ff3d274b9432d
larce <luis.arce22@gmail.com>
2018-01-07 21:45:57 -0400

    Example get resume

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/revel/revel"
+	"rb-rgo/app/models"
 )
 
 type App struct {
@@ -10,4 +11,9 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (c App) Resume() revel.Result {
+	res := &models.Resume{Id: "1", Title: "Full Stack Developer", Summary: "Summary"}
+	return c.RenderJSON(res)
 }
---
+++ app/models/resume.go
@@ -0,0 +1,7 @@
+package models
+
+type Resume struct {
+	Id       string `json:"id"`
+	Title string `json:"title"`
+	Summary string `json:"summary"`
+}
