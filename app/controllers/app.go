5d389f9da6b1d5883da41d7ee2b5cd8b00a1852e
larce <luis.arce22@gmail.com>
2018-01-07 21:58:22 -0400

    Return a list of resumes

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -5,6 +5,8 @@ import (
 	"rb-rgo/app/models"
 )
 
+type Queue []*models.Resume
+
 type App struct {
 	*revel.Controller
 }
@@ -13,7 +15,14 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Resume() revel.Result {
+func (c App) ResumeList() revel.Result {
+	var q Queue
+	resume := &models.Resume{Id: "1", Title: "Full Stack GO Developer", Summary: "Summary"}
+	q = append(q, resume)
+	return c.RenderJSON(q)
+}
+
+func (c App) ResumeById() revel.Result {
 	res := &models.Resume{Id: "1", Title: "Full Stack Developer", Summary: "Summary"}
 	return c.RenderJSON(res)
 }
