0f385f697fa00a40ebfb934c49fb99b2fb855259
larce <luis.arce22@gmail.com>
2018-01-07 22:03:53 -0400

    Return a list of resumes

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -17,12 +17,14 @@ func (c App) Index() revel.Result {
 
 func (c App) ResumeList() revel.Result {
 	var q Queue
-	resume := &models.Resume{Id: "1", Title: "Full Stack GO Developer", Summary: "Summary"}
-	q = append(q, resume)
+	resume0 := &models.Resume{Id: "1", Title: "Full Stack GO Developer", Summary: "Summary"}
+	q = append(q, resume0)
+	resume1 := &models.Resume{Id: "2", Title: "Revel Developer", Summary: "Summary"}
+	q = append(q, resume1)
 	return c.RenderJSON(q)
 }
 
-func (c App) ResumeById() revel.Result {
+func (c App) ResumeById(id string) revel.Result {
 	res := &models.Resume{Id: "1", Title: "Full Stack Developer", Summary: "Summary"}
 	return c.RenderJSON(res)
 }
